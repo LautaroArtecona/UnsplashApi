cae36e05407262db3b5c0f9179fb54708080363a
Lautaro Artecona <lautaroartecona@gmail.com>
2023-02-20 08:28:41 -0300

    config la pagina carga mas de 10 fotos

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,20 @@ function App() {
 
   useEffect(() => {
     const obtenerfotos=async ()=>{
-      const response=await axios.get('https://api.unsplash.com/photos/?client_id=dyqKJjrsm1oZS_XJqTv8aoKa3-mz6pTdAADYWWpC2O4')
+      const response=await axios.get('https://api.unsplash.com/photos/?client_id=dyqKJjrsm1oZS_XJqTv8aoKa3-mz6pTdAADYWWpC2O4&per_page=50')
       setFotos(response.data)}
     obtenerfotos()
+/*     console.log(fotos.map(foto=>(foto.urls.small))) */
+/*       console.log(fotos) */
 })
   return (
     <div className='container'>
-    <div className='container-img'>
-      {fotos.map(foto=>(
-      <div key={foto.id}>
-        <img src={foto.urls.small} alt=''/>
-      </div>))}
-    </div>
+      <div className='container-img'>
+        {fotos.map(foto=>(
+        <div key={foto.id}>
+          <img src={foto.urls.small} alt=''/>
+        </div>))}
+      </div>
     </div>
   );
 }
