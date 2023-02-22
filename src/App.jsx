917212f3232bc1947cf450f7fcb47d88e8b52c0b
Lautaro Artecona <lautaroartecona@gmail.com>
2023-02-22 14:56:21 -0300

    scroll infinito creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import './App.css';
 import axios from 'axios'
 import {useEffect, useState} from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 function App() {
 
   const [fotos,setFotos]=useState([])
+  const [pagina,setPagina]=useState([1])
 
   useEffect(() => {
     const obtenerfotos=async ()=>{
-      const response=await axios.get('https://api.unsplash.com/photos/?client_id=dyqKJjrsm1oZS_XJqTv8aoKa3-mz6pTdAADYWWpC2O4&per_page=50')
-      setFotos(response.data)}
+      const response=await axios.get('https://api.unsplash.com/photos/?client_id=dyqKJjrsm1oZS_XJqTv8aoKa3-mz6pTdAADYWWpC2O4&per_page=9&page='+ pagina)
+      setFotos((paginaPrev) => paginaPrev.concat(response.data))}
     obtenerfotos()
 /*     console.log(fotos.map(foto=>(foto.urls.small))) */
 /*       console.log(fotos) */
-})
+},[pagina]);
   return (
-    <div className='container'>
-      <div className='container-img'>
-        {fotos.map(foto=>(
-        <div key={foto.id}>
-          <img src={foto.urls.small} alt=''/>
-        </div>))}
+  <InfiniteScroll 
+    dataLength={fotos.length} 
+    hasMore={true} 
+    next={() => setPagina ((paginaPrev) => paginaPrev + 1)}>
+      <div className='container'>
+        <div className='container-img'>
+          {fotos.map(foto=>(
+          <div key={foto.id}>
+            <img src={foto.urls.small} alt=''/>
+          </div>))}
+        </div>
       </div>
-    </div>
+  </InfiniteScroll>
   );
 }
 
