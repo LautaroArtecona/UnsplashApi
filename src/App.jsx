55759454fa3f63a5696d0440bf5ba9715bcfd21b
Lautaro Artecona <lautaroartecona@gmail.com>
2023-02-27 23:45:45 -0300

    aplicacion de los primeros estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,65 @@
 import './App.css';
-import axios from 'axios'
 import {useEffect, useState} from 'react'
-import InfiniteScroll from 'react-infinite-scroll-component'
 import {Buscador} from './components';
 
 function App() {
 
   const [fotos,setFotos]=useState([])
-  const [pagina,setPagina]=useState([1])
+  const [pagina,setPagina]=useState(1)
+
 
   useEffect(() => {
     const obtenerfotos=async ()=>{
-      const response=await axios.get('https://api.unsplash.com/photos/?client_id=dyqKJjrsm1oZS_XJqTv8aoKa3-mz6pTdAADYWWpC2O4&per_page=9&page='+ pagina)
-      setFotos((paginaPrev) => paginaPrev.concat(response.data))}
-    obtenerfotos()
-/*     console.log(fotos.map(foto=>(foto.urls.small))) */
-       
-},[pagina]);
+      try{
+        const res=await fetch(`https://api.unsplash.com/photos/?client_id=dyqKJjrsm1oZS_XJqTv8aoKa3-mz6pTdAADYWWpC2O4&page=${pagina}`)
+        const fotos=await res.json()
+        setFotos(pre=>[...pre, ...fotos])
+      }catch(error){
+        console.log(error)
+      }
+    }
+      obtenerfotos()
+    },[pagina])
+
+  useEffect(()=>{
+    const handleScroll=(e)=> {
+      const scrollHeight=e.target.documentElement.scrollHeight
+      const currentHeight=e.target.documentElement.scrollTop + window.innerHeight
+      if (currentHeight + 1 >= scrollHeight) {
+        setPagina(pagina+5)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+    return ()=> window.removeEventListener("scroll", handleScroll)
+  }, [pagina])
+
+    function fotosFilt(foto) {
+
+    return foto.user.location!= null && foto.user.username!=null;
+    
+  }
+
+  const fotosFiltradas= fotos.filter(fotosFilt)
 
   return (
-  <InfiniteScroll 
-    dataLength={fotos.length} 
-    hasMore={true} 
-    next={() => setPagina ((paginaPrev) => paginaPrev + 1)}>
       <div className='container'>
-        <Buscador/>
-        <div className='row'>
-          {fotos.map(foto=>(
-          <div key={foto.id} className="col-4">
-            <img src={foto.urls.small} alt=''/>
-          </div>
-          ))}
+        <div className='cont-busca'>
+          <Buscador/>
+        </div> 
+        <div className='row justify-content-around'>
+          {fotosFiltradas && fotosFiltradas.map(foto=>(
+          <div className='card col-sm-3 cont-tarj' key={foto.id} /* style="width: 18rem;" */ >
+              <img src={foto.urls.small} className='card-img-top fotos' alt="" />
+              <div className='card-body tarjeta'>
+                <h5 className='card-title descrip'><strong>Descripcion:</strong> {fotos.description!=null ? fotos.description : foto.alt_description}</h5>
+                <div>
+                  <p className='card-text username'><strong>Usuario:</strong> {foto.user.username}</p>
+                  <p className='card-text location'><strong>Ubicacion:</strong> {foto.user.location}</p>
+                </div>
+              </div>
+          </div>))}
         </div>
       </div>
-  </InfiniteScroll>
   );
 }
 
--- src/components/buscador/buscador.jsx
+++ src/components/buscador/buscador.jsx
@@ -3,10 +3,11 @@ import './Buscador.css';
 
 const Buscador = () => {
   return (
-    <form action="" className='cont-buscador'>
+    <form action="" className='cont-buscador shadow p-3 mb-5 bg-body-tertiary rounded'>
         <input type="text" placeholder='Buscar' className='buscador'/>
         <i className="bi bi-search lupa"></i>
     </form>
+    
   )
 }
 
